62992966078872bb55f63d15f620ca7e0eb09ed0
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-15 09:42:59 +0530

    fix :fixed code for production

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -10,18 +10,20 @@ interface SocketProviderProps {
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [connected, setConnected] = useState(false);
-  const currentUser = useGetUser()
+  const currentUser = useGetUser();
 
   useEffect(() => {
     //initialize the socket with url of backend
-    const newSocket = io(`${import.meta.env.VITE_BASE_URL}`,{
-      query : { userId : currentUser.id}
+    console.log(import.meta.env.VITE_BASE_URL)
+    const newSocket = io(`${import.meta.env.VITE_BASE_URL}`, {
+      withCredentials: true,
+      query: { userId: currentUser.id },
     });
     setSocket(newSocket);
     //calling the connect event for connect to server
     newSocket.on("connect", () => {
       console.log("client connected to server ===> ID ===> ", newSocket.id);
-      setConnected(true); 
+      setConnected(true);
     });
     //cleaner function for clean the unnessary mount
     return () => {
@@ -31,9 +33,9 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
   if (!connected) {
     console.log("not connecteddd run");
-    
+
     return null; // Or some loading indicator
-  } 
+  }
 
   return (
     <SocketContext.Provider value={{ socket }}>
