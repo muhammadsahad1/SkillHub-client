1eabd2efa15c90386d3a8c6773a0b45637bbb875
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-23 10:27:58 +0530

    fix : reusable table

--- src/components/common/ReusableTable.tsx
+++ src/components/common/ReusableTable.tsx
@@ -31,66 +31,68 @@ const ReusableTable: React.FC<ReusableTableProps> = ({
 
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full table-auto">
-        <thead>
-          <tr className="bg-zinc-950">
-            {columns.map((column) => (
-              <th
-                key={column.accessor}
-                className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
-              >
-                {column.Header}
-              </th>
-            ))}
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-200 uppercase tracking-wider">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        { data.length <= 0 ? (
-          <div className="flex justify-center items-center"> 
-            <p> No reports </p>
-          </div>
-        ) : (
-          <tbody className="bg-white divide-y divide-gray-200">
-          {currentData.map((item) => (
-            <tr key={item._id}>
-              {columns.map((column) => (
-                <td
-                  key={column.accessor}
-                  className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                >
-                  {item[column.accessor]}
-                </td>
+      {data.length <= 0 ? (
+        <div className="flex justify-center items-center">
+          <p>No reports </p>
+        </div>
+      ) : (
+        <>
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr className="bg-zinc-950">
+                {columns.map((column) => (
+                  <th
+                    key={column.accessor}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                  >
+                    {column.Header}
+                  </th>
+                ))}
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-200 uppercase tracking-wider">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white divide-y divide-gray-200">
+              {currentData.map((item) => (
+                <tr key={item._id}>
+                  {columns.map((column) => (
+                    <td
+                      key={column.accessor}
+                      className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                    >
+                      {item[column.accessor]}
+                    </td>
+                  ))}
+                  <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                    {renderActions(item)}
+                  </td>
+                </tr>
               ))}
-              <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                {renderActions(item)}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-        )}
-      
-      </table>
-      <div className="flex justify-between mt-4">
-        <button
-          disabled={currentPage === 1}
-          onClick={() => handlePageChange(currentPage - 1)}
-          className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
-        >
-          Previous
-        </button>
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          disabled={currentPage === totalPages}
-          onClick={() => handlePageChange(currentPage + 1)}
-          className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
-        >
-          Next
-        </button>
-      </div>
+            </tbody>
+          </table>
+          <div className="flex justify-between mt-4">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => handlePageChange(currentPage - 1)}
+              className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
+            >
+              Previous
+            </button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => handlePageChange(currentPage + 1)}
+              className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
